9bf8e323bf07d67074ef10111d20df88c097e3af
Jeanpierre23Web <129546357+Jeanpierre23Web@users.noreply.github.com>
2023-09-21 19:48:04 -0300

    Api-Transito
    Api-Transito. Version Definitiva

---
+++ database/factories/CamionerosFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CamionerosFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'id int primary key auto_increment' => $this -> faker-> numberId(),
+            'nombre' => $this->faker->firstName(),
+            'apellido' => $this->faker->lastName(),
+            'Camiones' => $this ->faker -> numberID(),
+            'Direccion'=> $this ->faker-> direccion_destino(),
+            'update_datetime' => $this->faker(),
+            'created_at datetime'=> $this ->faker()
+        ];
+    }
+}
